e07e8f69fee63bcf39d7a4f0f05cfe4443ec3189
DJthefirst <djberry8@ptd.net>
2025-06-30 20:19:57 -0400

    Completed initial GUI Layout

--- AvaloniaGUI/Factories/PageFactory.cs
+++ AvaloniaGUI/Factories/PageFactory.cs
@@ -1,18 +1,18 @@
-﻿// using AvaloniaGUI.Data;
+﻿using AvaloniaGUI.Data;
 using AvaloniaGUI.ViewModels;
 using System;
 
 namespace AvaloniaGUI.Factories;
 
-//public class PageFactory(Func<Type, PageViewModel> factory)
-//{
-//    public PageViewModel GetPageViewModel<T>(Action<T>? afterCreation = null)
-//        where T : PageViewModel
-//    {
-//        var viewModel = factory(typeof(T));
-        
-//        afterCreation?.Invoke((T)viewModel);
-        
-//        return viewModel;
-//    }
-//}
+public class PageFactory(Func<Type, PageViewModel> factory)
+{
+    public PageViewModel GetPageViewModel<T>(Action<T>? afterCreation = null)
+        where T : PageViewModel
+    {
+        var viewModel = factory(typeof(T));
+
+        afterCreation?.Invoke((T)viewModel);
+
+        return viewModel;
+    }
+}
--- AvaloniaGUI/ViewModels/MainViewModel.cs
+++ AvaloniaGUI/ViewModels/MainViewModel.cs
@@ -1,6 +1,58 @@
-﻿namespace AvaloniaGUI.ViewModels;
+﻿using AvaloniaGUI.Data;
+using AvaloniaGUI.Factories;
+using AvaloniaGUI.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 
-public partial class MainViewModel : ViewModelBase
+namespace AvaloniaGUI.ViewModels;
+
+public partial class MainViewModel : ViewModelBase, IDialogProvider
 {
-    public string Greeting => "Welcome to Avalonia!";
+	private readonly PageFactory _pageFactory;
+	//private readonly DatabaseFactory _databaseFactory;
+
+	[ObservableProperty]
+	private bool _sideMenuExpanded = true;
+
+
+	//[NotifyPropertyChangedFor(nameof(ProcessPageIsActive))]
+	//[NotifyPropertyChangedFor(nameof(ActionsPageIsActive))]
+	//[NotifyPropertyChangedFor(nameof(MacrosPageIsActive))]
+	//[NotifyPropertyChangedFor(nameof(ReporterPageIsActive))]
+	//[NotifyPropertyChangedFor(nameof(HistoryPageIsActive))]
+	//[NotifyPropertyChangedFor(nameof(SettingsPageIsActive))]
+
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(HomePageIsActive))]
+	private PageViewModel _currentPage;
+
+	//[ObservableProperty]
+	//private DialogViewModel _dialog;
+
+	public bool HomePageIsActive => CurrentPage.PageName == ApplicationPageNames.Home;
+	//public bool ProcessPageIsActive => CurrentPage.PageName == ApplicationPageNames.Process;
+	//public bool ActionsPageIsActive => CurrentPage.PageName == ApplicationPageNames.Actions;
+	//public bool MacrosPageIsActive => CurrentPage.PageName == ApplicationPageNames.Macros;
+	//public bool ReporterPageIsActive => CurrentPage.PageName == ApplicationPageNames.Reporter;
+	//public bool HistoryPageIsActive => CurrentPage.PageName == ApplicationPageNames.History;
+	//public bool SettingsPageIsActive => CurrentPage.PageName == ApplicationPageNames.Settings;
+
+	/// <summary>
+	/// Design-time only constructor
+	/// </summary>
+	// Allow nullable PageFactory for now in designer... ideally get it working
+#pragma warning disable CS8618, CS9264
+	public MainViewModel()
+	{
+		//CurrentPage = new SettingsPageViewModel(new DatabaseFactory(() => new DatabaseService(new ApplicationDbContext())));
+		CurrentPage = new HomePageViewModel();
+	}
+#pragma warning restore CS8618, CS9264
+
+	[RelayCommand]
+	private void SideMenuResize() => SideMenuExpanded = !SideMenuExpanded;
+
+	[RelayCommand]
+	private void GoToHome() => CurrentPage = _pageFactory.GetPageViewModel<HomePageViewModel>();
 }
