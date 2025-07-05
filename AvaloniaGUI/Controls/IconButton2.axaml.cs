32de8dfd68794bebd24b4a963acc1b5c7096ea31
DJthefirst <djberry8@ptd.net>
2025-07-05 17:55:22 -0400

    Added Icons

---
+++ AvaloniaGUI/Controls/IconButton2.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using System;
+
+namespace AvaloniaGUI.Controls;
+
+public class IconButton2 : Button
+{
+
+}
---
+++ AvaloniaGUI/ViewModels/MidiPlayerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AvaloniaGUI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaGUI.ViewModels;
+
+public partial class MidiPlayerViewModel() : ViewModelBase
+{
+
+}
